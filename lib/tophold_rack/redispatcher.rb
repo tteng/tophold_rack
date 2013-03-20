5bfab8775f1c387e3f7ffc8bdb04e83d366e67d9
readline <tim.rubist@gmail.com>
2013-03-20 16:36:35 +0800

    debug

--- lib/tophold_rack/redispatcher.rb
+++ lib/tophold_rack/redispatcher.rb
@@ -9,13 +9,13 @@ module TopholdRack
     def call env
       request = Rack::Request.new env
       if request.get?
-        p "request: #{request.to_s}"
+        p "request: #{request.body}"
         path, params = request.path, request.params
         p "path: #{request.path}" 
         p "params: #{request.params}"
         scope = env["rack.session"]["warden.user.#{Rails.configuration.tophold_rack_devise_scope}.key"]
         user_id = scope[1][0] if scope
-        unless path =~ /^(#{Rails.configuration.tophold_rack_request_black_list.join('|')})/
+        unless path =~ /\/^(#{Rails.configuration.tophold_rack_request_black_list.join('|')})/
           p "passed..."
         end
       end
