0d1dd360a480b426708155f739c74a5fb9832660
readline <tim.rubist@gmail.com>
2013-03-20 15:57:16 +0800

    debug

--- lib/tophold_rack/redispatcher.rb
+++ lib/tophold_rack/redispatcher.rb
@@ -10,12 +10,7 @@ module TopholdRack
       request = Rack::Request.new env
       p "path: #{request.path}" 
       p "params: #{request.params}"
-      env["rack.session"].keys.sort.each do |k|
-        p k 
-        p env["rack.session"][k].inspect
-      end
-      p "warden.user.#{Rails.configuration.tophold_rack_devise_scope}"
-      scope = env["rack.session"]["warden.user.#{Rails.configuration.tophold_rack_devise_scope}"]
+      scope = env["rack.session"]["warden.user.#{Rails.configuration.tophold_rack_devise_scope}".key]
       if scope
         p scope[1].inspect 
       else
